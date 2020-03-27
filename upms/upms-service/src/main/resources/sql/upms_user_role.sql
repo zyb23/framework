6c0bd3ca22e881228b2ebc4c5f559a29562888ff
zyb23 <615794063@qq.com>
2020-03-27 13:43:47 +0800

    OrderBuilder CRC32

--- upms/upms-service/src/main/resources/sql/upms_user_role.sql
+++ upms/upms-service/src/main/resources/sql/upms_user_role.sql
@@ -2,12 +2,10 @@ CREATE TABLE `upms_user_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) NOT NULL,
   `role_id` bigint(20) NOT NULL,
-  `create_time` datetime NOT NULL,
-  `creator_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UKmjo3l593cr1835oo1asuch8u` (`user_id`,`role_id`),
   KEY `FKhh52n8vd4ny9ff4x9fb8v65qx` (`role_id`),
   CONSTRAINT `FKb40xxfch70f5qnyfw8yme1n1s` FOREIGN KEY (`user_id`) REFERENCES `upms_user` (`id`),
   CONSTRAINT `FKhh52n8vd4ny9ff4x9fb8v65qx` FOREIGN KEY (`role_id`) REFERENCES `upms_role` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-INSERT INTO `upms_user_role`(`id`, `user_id`, `role_id`, `create_time`, `creator_id`) VALUES (1, 0, 0, '2019-12-12 14:04:22', -1);
+INSERT INTO `upms_user_role`(`id`, `user_id`, `role_id`) VALUES (1, 0, 0);
