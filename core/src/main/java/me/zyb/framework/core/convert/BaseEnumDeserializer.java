65e4c86b37ed51e9eb7a58dc35e2eed8a06ebc69
zyb23 <615794063@qq.com>
2020-04-27 13:08:46 +0800

    枚举转换，支持value值与对象形式

--- core/src/main/java/me/zyb/framework/core/convert/BaseEnumDeserializer.java
+++ core/src/main/java/me/zyb/framework/core/convert/BaseEnumDeserializer.java
@@ -1,10 +1,12 @@
 package me.zyb.framework.core.convert;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
 import me.zyb.framework.core.base.BaseEnum;
 import org.springframework.beans.BeanUtils;
 
@@ -14,11 +16,12 @@ import java.io.IOException;
  * json 枚举转化（反序列化）
  * @author zhangyingbin
  */
+@Slf4j
 public class BaseEnumDeserializer extends JsonDeserializer<BaseEnum> {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public BaseEnum deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+	public BaseEnum deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
 		JsonNode node = jp.getCodec().readTree(jp);
 		//枚举的参数名
 		String propertyName = jp.currentName();
@@ -26,6 +29,14 @@ public class BaseEnumDeserializer extends JsonDeserializer<BaseEnum> {
 		Object currentValue = jp.getCurrentValue();
 		Class enumType = BeanUtils.findPropertyType(propertyName, currentValue.getClass());
 
-		return (BaseEnum) BaseEnum.getEnum(node.asText(), enumType);
+		JSONObject jb = null;
+		try {
+			jb = JSON.parseObject(node.toString());
+		} catch (Exception e){
+			log.info("前端传递的是枚举值，不是枚举对象");
+		}
+		String value = null == jb ? node.asText() : jb.getString("value");
+
+		return (BaseEnum) BaseEnum.getEnum(value, enumType);
 	}
 }
