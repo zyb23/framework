68c88ece8858e5443f52af145b4ef402870e3242
zyb23 <615794063@qq.com>
2020-05-07 18:03:28 +0800

    消息队列

--- upms/upms-api/src/main/java/me/zyb/framework/upms/dict/UpmsRabbitMqConst.java
+++ upms/upms-api/src/main/java/me/zyb/framework/upms/dict/UpmsRabbitMqConst.java
@@ -5,9 +5,10 @@ package me.zyb.framework.upms.dict;
  * @author zhangyingbin
  */
 public class UpmsRabbitMqConst {
+	/** UPMS 主题交换机 */
+	public static final String EXCHANGE_TOPIC_UPMS = "exchange.topic.upms";
 	/** UPMS 日志队列 */
 	public static final String QUEUE_UPMS_LOG = "queue.upms.log";
-
 	/** UPMS 日志路由键 */
 	public static final String ROUTING_KEY_UPMS_LOG = "routing.key.upms.log";
 }
